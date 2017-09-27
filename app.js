79a9b31fccb960c47057b3a16c8b236e39c1908d
Paton Vinal <paton.vinal@gmail.com>
2017-09-27 17:33:16 -0400

    removed submit form and corresponding css

--- app.js
+++ app.js
@@ -2,49 +2,33 @@
 
 const appState ={
   profileToggle: false,
-  contactToggle: false,
   portfolioToggle: false,
   defaultToggle: true
 };
 
 function changeDefault(state){
   state.portfolioToggle = false;
-  state.contactToggle = false;
   state.profileToggle = false;
   return state.defaultToggle = true;
 }
 
 function changeProfile(state){
   state.portfolioToggle = false;
-  state.contactToggle = false;
   state.defaultToggle = false;
   return state.profileToggle = true;
 }
 
-function changeContact(state){
-  state.portfolioToggle = false;
-  state.profileToggle = false;
-  state.defaultToggle = false;
-  return state.contactToggle = true;
-}
-
 function changePortfolio(state){
   state.profileToggle = false;
-  state.contactToggle = false;
   state.defaultToggle = false;
   return state.portfolioToggle = true;
 }
 
-function render(state, profile, contact, portfolio, main){
+function render(state, profile, portfolio, main){
   if(state.profileToggle){
     profile.show();
   }else{
     profile.hide();
-  }
-  if(state.contactToggle){
-    contact.show();
-  }else{
-    contact.hide();
   }
   if(state.portfolioToggle){
     portfolio.show();
@@ -58,25 +42,20 @@ function render(state, profile, contact, portfolio, main){
 }
 
 $(function(){
-  render(appState, $('.profile-info'), $('.contact-info'), $('.portfolio-info'), $('.default-body'));
+  render(appState, $('.profile-info'), $('.portfolio-info'), $('.default-body'));
 
   $('.profile').on('click', function(event){
     changeProfile(appState);
-    render(appState, $('.profile-info'), $('.contact-info'), $('.portfolio-info'), $('.default-body'));
-  });
-
-  $('.contact').on('click', function(event){
-    changeContact(appState);
-    render(appState, $('.profile-info'), $('.contact-info'), $('.portfolio-info'), $('.default-body'));
+    render(appState, $('.profile-info'), $('.portfolio-info'), $('.default-body'));
   });
 
   $('.portfolio').on('click', function(event){
     changePortfolio(appState);
-    render(appState, $('.profile-info'), $('.contact-info'), $('.portfolio-info'), $('.default-body'));
+    render(appState, $('.profile-info'), $('.portfolio-info'), $('.default-body'));
   });
 
   $('.logo').on('click', function(event){
     changeDefault(appState);
-    render(appState, $('.profile-info'), $('.contact-info'), $('.portfolio-info'), $('.default-body'));
+    render(appState, $('.profile-info'), $('.portfolio-info'), $('.default-body'));
   });
 });
